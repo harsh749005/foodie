0122e2d247a94069f44981cd0aabc1f068f0df49
Harsh <patelharsh749005@gmail.com>
2024-12-26 23:45:13 +0530

    UI has been fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,15 +16,16 @@ const Card = () => {
         </div>
         <div className="p-5 flex flex-col gap-2">
           <div className="flex items-center justify-between">
-            <h2 className="text-xl font-semibold">{item.name}</h2>
-            <img src={assets.rating_starts} alt="" />
+            <h2 className="flex items-center w-40 h-14 text-lg font-semibold leading-1">{item.name}</h2>
+            {/* <img className="w-16" src={assets.rating_starts} alt="" /> */}
+            <div className="bg-orange-600 rounded text-white font-medium pl-2 pr-1">4.5<small>⭐</small></div>
           </div>
-          <p className="text-gray-600">{item.description}</p>
-          <div className="mt-5 flex justify-between items-center">
-            <h2 className="text-red-600 font-medium text-2xl ">
+          <p className="text-gray-600 h-[70px] ">{item.description}</p>
+          <div className="mt-2 flex justify-between items-center">
+            <h2 className="text-orange-600 font-bold text-2xl ">
               ${item.price}
             </h2>
-            <div className="rounded-full w-10 h-10 border-2 border-salte-500 p-2 ">
+            <div className="rounded-full w-8 h-8 border-2 bg-orange-300 border-orange-500 p-2 ">
               <img
                 src={assets.plus}
                 alt=""
