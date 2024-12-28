80924c4ce65900c9ae01d66298cf77897c175e13
Harsh <patelharsh749005@gmail.com>
2024-12-28 22:21:37 +0530

    Text chnaged

--- src/Pages/Orders.jsx
+++ src/Pages/Orders.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { assets } from '../assets/frontend_assets/assets'
+import Footer from '../components/Footer'
 const Orders = () => {
   return (
+    <div>
     <div className='w-[90%] xl:w-[70%] m-auto flex flex-col gap-5'>
       <h1 className='text-2xl font-medium'>My Orders</h1>
       <div className='h-64 md:h-max flex flex-col justify-between  md:flex-row md:justify-between md:items-center border-2 border-orange-400 rounded p-2'>
@@ -15,12 +17,14 @@ const Orders = () => {
             <div className='w-2 h-2'>
                 <img src={assets.selector_icon} alt="" />
             </div>
-            <p>Food Processing</p>
+            <p>Order Processing</p>
         </div>
         <div className='bg-orange-400 w-40 md:w-auto md:h-max text-center p-2 md:p-3 md:pl-4 md:pr-4 rounded text-white font-medium'>
             Tack Order
         </div>
       </div>
+    </div>
+      <Footer/>
     </div>
   )
 }
